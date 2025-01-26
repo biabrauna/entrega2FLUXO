8c655d39a1be6c708df701ad780c20f9e27e8494
Bia Brauna <167824411+biabrauna@users.noreply.github.com>
2025-01-25 22:21:09 -0300

    fixing errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync")
 
 class Reserva{
-    construtor(id_cliente, status, data_entrada, data_saida){
+    constructor(id_cliente, status, data_entrada, data_saida){
         this.id = getId();
         this.id_cliente = id_cliente;
         this.status = status;
@@ -16,7 +16,7 @@ class Reserva{
 }
 
 class Funcionario{
-    construtor(nome, cpf, email, senha){
+    constructor(nome, cpf, email, senha){
         this.id = getId();
         this.nome = nome;
         this.cpf = cpf;
@@ -45,7 +45,7 @@ class Cliente{
 }
 
 class Quartos{
-    construtor(){
+    constructor(){
         this.quantidade_camas = quantidade_camas;
         this.preco_noite = preco_noite;
         this.quantidade_disponivel = quantidade_disponivel;
@@ -55,7 +55,7 @@ class Quartos{
 }
 
 class Sistema{
-    construtor(){ 
+    constructor(){ 
         this.funcionarios = [];
         this.clientes = [];
         this.quartos = [];
@@ -72,7 +72,7 @@ class Sistema{
             const opcao = readline.questionInt("Digite a opcao desejada: ");
             switch(opcao){
                 case 1:
-                    sistema.login();
+                    this.login();
                     break;
                 case 2:
                     this.cadastro();
@@ -108,7 +108,7 @@ class Sistema{
             if(this.clientes[i].email === email && this.clientes[i].senha === senha){
                 console.log("Login efetuado com sucesso");
                 console.log("Bem vindo ", this.clientes[i].nome);
-                tela_funcionario(this.clientes[i]);
+                this.tela_cliente(this.clientes[i]);
             }
         }
         console.log("Login inválido");
@@ -118,34 +118,34 @@ class Sistema{
             if(this.funcionarios[i].email === email && this.funcionarios[i].senha === senha){
                 console.log("Login efetuado com sucesso");
                 console.log("Bem vindo ", this.funcionarios[i].nome);
-                tela_cliente(this.funcionarios[i])
+                this.tela_funcionario(this.funcionarios[i])
             }
         }
         console.log("Login inválido");
     }
-}
+    }
 
-cadastro(){
-    console.log("Quem você é?");
-    console.log("1 - Cliente");
-    console.log("2 - Funcionário"); 
-    const opcao = readline.questionInt("Digite a opcao desejada: ");
-    const nome = readline.question("Digite seu nome: ");
-    const cpf = readline.question("Digite seu cpf: "); 
-    const email = readline.question("Digite seu email: ");
-    const senha = readline.question("Digite sua senha: ");
-    if(opcao === 1){
+    cadastro(){
+        console.log("Quem você é?");
+        console.log("1 - Cliente");
+        console.log("2 - Funcionário"); 
+        const opcao = readline.questionInt("Digite a opcao desejada: ");
+        const nome = readline.question("Digite seu nome: ");
+        const cpf = readline.question("Digite seu cpf: "); 
+        const email = readline.question("Digite seu email: ");
+        const senha = readline.question("Digite sua senha: ");
+        if(opcao === 1){
             const nascimento = readline.question("Digite sua data de nascimento: ");
             const cliente = new Cliente(nome, cpf, email, senha, nascimento);
             console.log(cliente);
-            sistema.clientes.push(cliente);
+            this.clientes.push(cliente);
         }
-    if(opcao === 2){
+        if(opcao === 2){
             this.funcionarios.push(new Funcionario(nome, cpf, email, senha));
             console.log(this.funcionarios);
-        }
+            }
     }
-    
+        
     tela_funcionario(funcionario){
         console.log("1 - Ver meus Dados")
         console.log("2 - Ver Lista de Quartos")
@@ -156,19 +156,19 @@ cadastro(){
         const opcao = readline.questionInt("Digite a opcao desejada: ");
         switch(opcao){
             case 1:
-                sistema.ver_dados_funcionario(funcionario);
+                this.ver_dados_funcionario(funcionario);
                 break;
             case 2:
-                sistema.ver_lista_quartos(quartos);
+                this.ver_lista_quartos();
                 break;
             case 3:
-                sistema.ver_lista_clientes(clientes);
+                this.ver_lista_clientes();
                 break;
             case 4:
-                sistema.ver_lista_reservas(reservas);
+                this.ver_lista_reservas();
                 break;
             case 5:
-                sistema.mudar_status_reserva(reservas);
+                this.mudar_status_reserva();
                 break;
             case 6:
                 console.log("Obrigado por usar o sistema de reservas");
@@ -179,48 +179,49 @@ cadastro(){
         }
     }
 
-    
     tela_cliente(cliente){
-        console.log("1 - Ver meus Dados")
-        console.log("2 - Ver Lista de Quartos")
-        console.log("3 - Fazer reserva");
-        console.log("4 - Cancelar reserva");
-        console.log("5 - Verificar reserva");
-        console.log("6 - Sair do programa");
+        let t = true
+        while(t === true){
+            console.log("1 - Ver meus Dados")
+            console.log("2 - Ver Lista de Quartos")
+            console.log("3 - Fazer reserva");
+            console.log("4 - Cancelar reserva");
+            console.log("5 - Verificar reserva");
+            console.log("6 - Sair do programa");
         const opcao = readline.questionInt("Digite a opcao desejada: ");
         switch(opcao){
             case 1:
-                sistema.ver_dados_cliente(cliente);
+                this.ver_dados_cliente(cliente);
                 break;
             case 2:
-                sistema.ver_lista_quartos(quartos);
+                this.ver_lista_quartos();
                 break;
             case 3:
-                sistema.fazer_reserva(reservas);
+                this.fazer_reserva();
                 break;
             case 4:
-                sistema.cancelar_reserva(reservas);
+                this.cancelar_reserva();
                 break;
             case 5:
-                sistema.verificar_reserva(reservas);
+                this.verificar_reserva();
                 break;
             case 6:
-                sistema.console.log("Obrigado por usar o sistema de reservas");
+                console.log("Obrigado por usar o sistema de reservas");
                 break;
             default:
                 console.log("Opção inválida");
                 break;
+            }
         }
     }
 
-
-    ver_lista_quartos(quartos){
-        for(i = 0; i < quartos.length; i++){
-            console.log("Quarto: ", quartos[i].nome);
-            console.log("Descrição: ", quartos[i].descricao);
-            console.log("Preço por noite: ", quartos[i].preco_noite);
-            console.log("Quantidade de camas: ", quartos[i].quantidade_camas);
-            console.log("Quantidade disponível: ", quartos[i].quantidade_disponivel);
+    ver_lista_quartos(){
+        for(i = 0; i < this.quartos.length; i++){
+            console.log("Quarto: ", this.quartos[i].nome);
+            console.log("Descrição: ", this.quartos[i].descricao);
+            console.log("Preço por noite: ", this.quartos[i].preco_noite);
+            console.log("Quantidade de camas: ", this.quartos[i].quantidade_camas);
+            console.log("Quantidade disponível: ", this.quartos[i].quantidade_disponivel);
         }
     }
 
@@ -230,35 +231,35 @@ cadastro(){
         const data_entrada = readline.question("Digite a data de entrada: ");
         const data_saida = readline.question("Digite a data de saída: ");
         const reserva = new Reserva(id_cliente, status, data_entrada, data_saida);
-        reservas.push(reserva);
+        this.reservas.push(reserva);
     }
 
     cancelar_reserva(reservas, cliente){
-        for(i = 0; i < reservas.length; i++){
-            if(reservas[i].id_cliente === cliente.id){
-                console.log("Reserva: ", reservas[i].id);
+        for(i = 0; i < this.reservas.length; i++){
+            if(this.reservas[i].id_cliente === cliente.id){
+                console.log("Reserva: ", this.reservas[i].id);
                 console.log("Cliente: ", cliente.nome);
-                console.log("Status: ", reservas[i].status);
-                console.log("Data de entrada: ", reservas[i].data_entrada);
-                console.log("Data de saída: ", reservas[i].data_saida);
+                console.log("Status: ", this.reservas[i].status);
+                console.log("Data de entrada: ", this.reservas[i].data_entrada);
+                console.log("Data de saída: ", this.reservas[i].data_saida);
             }}
             const id = readline.questionInt("Digite o id da reserva: ");
-            for(i = 0; i < reservas.length; i++){
-            if(reservas[i].id === id){
-                reservas.splice(i, 1);
+            for(i = 0; i < this.reservas.length; i++){
+            if(this.reservas[i].id === id){
+                this.reservas.splice(i, 1);
             }
         }
     }
 
-    verificar_reserva(reservas, cliente){
-        for(i = 0; i < reservas.length; i++){
-            if(reservas[i].id_cliente === cliente.id){
-                console.log("Cliente: ", cliente.nome);
-                console.log("Status: ", reservas[i].status);
-                console.log("Data de entrada: ", reservas[i].data_entrada);
-                console.log("Data de saída: ", reservas[i].data_saida);
+    verificar_reserva(cliente){
+        for(i = 0; i < this.reservas.length; i++){
+            if(this.reservas[i].id_cliente === cliente.id){
+                console.log("Cliente: ", this.cliente.nome);
+                console.log("Status: ", this.reservas[i].status);
+                console.log("Data de entrada: ", this.reservas[i].data_entrada);
+                console.log("Data de saída: ", this.reservas[i].data_saida);
             }}
-        }
+    }
 
     ver_dados_funcionario(funcionario){
         console.log("ID: ", funcionario.id);
@@ -268,28 +269,28 @@ cadastro(){
         console.log("Senha: ", funcionario.senha);
     }
 
-    ver_lista_clientes(clientes){
-        for(i = 0; i < clientes.length; i++){
-            console.log("ID: ", clientes[i].id);
-            console.log("Nome: ", clientes[i].nome);
-            console.log("CPF: ", clientes[i].cpf);
-            console.log("Email: ", clientes[i].email);
-            console.log("Data de Nascimento: ", clientes[i].nascimento);
+    ver_lista_clientes(){
+        for(i = 0; i < this.clientes.length; i++){
+            console.log("ID: ", this.clientes[i].id);
+            console.log("Nome: ", this.clientes[i].nome);
+            console.log("CPF: ", this.clientes[i].cpf);
+            console.log("Email: ", this.clientes[i].email);
+            console.log("Data de Nascimento: ", this.clientes[i].nascimento);
         }
     }
 
-    ver_lista_reservas(reservas){
-        for(i = 0; i < reservas.length; i++){
-            console.log("ID: ", reservas[i].id);
-            console.log("ID Cliente: ", reservas[i].id_cliente);
-            console.log("Status: ", reservas[i].status);
-            console.log("Data de Entrada: ", reservas[i].data_entrada);
-            console.log("Data de Saída: ", reservas[i].data_saida);
+    ver_lista_reservas(){
+        for(i = 0; i < this.reservas.length; i++){
+            console.log("ID: ", this.reservas[i].id);
+            console.log("ID Cliente: ", this.reservas[i].id_cliente);
+            console.log("Status: ", this.reservas[i].status);
+            console.log("Data de Entrada: ", this.reservas[i].data_entrada);
+            console.log("Data de Saída: ", this.reservas[i].data_saida);
         }
     }
 
-    mudar_status_reserva(reservas){
-        for(i = 0; i < reservas.length; i++){
+    mudar_status_reserva(){
+        for(i = 0; i < this.reservas.length; i++){
             console.log("ID: ", reservas[i].id);
             console.log("ID Cliente: ", reservas[i].id_cliente);
             console.log("Status: ", reservas[i].status);
@@ -298,14 +299,13 @@ cadastro(){
         }
         const id = readline.questionInt("Digite o id da reserva: ");
         const status = readline.question("Digite o novo status da reserva: ");
-        for(i = 0; i < reservas.length; i++){
-            if(reservas[i].id === id){
-                reservas[i].status = status;
+        for(i = 0; i < this.reservas.length; i++){
+            if(this.reservas[i].id === id){
+                this.reservas[i].status = status;
             }
         }
     }
 }
-
 
 const sistema = new Sistema()
 sistema.inicio()
